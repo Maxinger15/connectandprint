5bfd518e4d4547e86400cae5f449eeb342c4aace
Max Grallinger <gm@rebricker.com>
2023-04-29 10:55:31 +0200

    updated filename to match the id, added additional control properties

--- connectandprint.py
+++ connectandprint.py
@@ -6,7 +6,7 @@ import flask
 import time
 
 
-class AutoConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin, 
+class ConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin, 
                                 octoprint.plugin.RestartNeedingPlugin):
 
     def on_event(self, event, payload):
@@ -36,7 +36,7 @@ class AutoConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin,
 
     def get_update_information(self):
         return dict(
-            auto_connect_and_print=dict(
+            connectandprint=dict(
                 displayName=self._plugin_name,
                 displayVersion=self._plugin_version,
 
@@ -47,14 +47,18 @@ class AutoConnectAndPrintPlugin(octoprint.plugin.EventHandlerPlugin,
                 current=self._plugin_version,
 
                 # Update method: pip
-                pip="https://raw.githubusercontent.com/Maxinger15/connectandprint/{target_version}/connect_and_print.py"
+                pip="https://raw.githubusercontent.com/Maxinger15/connectandprint/{target_version}/connectandprint.py"
             )
         )
 
 __plugin_name__ = "Connect And Print"
 __plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_version__ = "1.0.0"
-__plugin_implementation__ = AutoConnectAndPrintPlugin()
+__plugin_description__='Automatically connect to your printer on file upload and start printing'
+__plugin_author__="Max Grallinger"
+__plugin_url__="https://github.com/Maxinger15/connectandprint"
+__plugin_license__="AGPLv3"
+__plugin_implementation__ = ConnectAndPrintPlugin()
 __plugin_hooks__ = {
     "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 }
